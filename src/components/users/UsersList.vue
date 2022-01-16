345f8ef398f61985314f6b5db211e052db35e6eb
Anand raja <meetanandns@gmail.com>
2022-01-16 12:24:33 +0000

    programatical route

--- src/components/users/UsersList.vue
+++ src/components/users/UsersList.vue
@@ -1,4 +1,5 @@
 <template>
+  <button @click="confirmInput">Confirm</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -12,6 +13,14 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  methods: {
+    confirmInput() {
+      // some actions
+      this.$router.push('/teams');
+      // this.$router.forward();
+      // this.$router.back();
+    }
+  }
 };
 </script>
 
